67ca12f882ff0c7006cb2daf9a29e533969b0f88
Ferenc Toth <toth_ferenc@hotmail.com>
2022-05-02 17:27:53 +0200

    initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { DashboardComponent } from './layout/components/dashboard/dashboard.component';
+import { AppMainComponent } from './layout/app-main/app.main.component';
+import { LoginComponent } from './layout/components/login/login.component';
+import { ErrorComponent } from './layout/components/error/error.component';
+import { NotfoundComponent } from './layout/components/notfound/notfound.component';
+import { AccessComponent } from './layout/components/access/access.component';
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot([
+            {
+                path: '', component: AppMainComponent,
+                children: [
+                    {path: '', component: DashboardComponent},
+                ],
+            },
+            {path:'pages/login', component: LoginComponent},
+            {path:'pages/error', component: ErrorComponent},
+            {path:'pages/notfound', component: NotfoundComponent},
+            {path:'pages/access', component: AccessComponent},
+            {path: '**', redirectTo: 'pages/notfound'},
+        ], {scrollPositionRestoration: 'enabled', anchorScrolling:'enabled'})
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
